93ee931d200db7c1cf90f113fd558e1545da0735
Tres Seaver <tseaver@palladion.com>
2025-11-07 17:56:37 -0500

    feat: 'example/rooms/faux'
    Demonstrates creating a faux agent class, to be wrapped in an
    AG-UI adapter.

--- src/soliplex/examples.py
+++ src/soliplex/examples.py
@@ -1,6 +1,18 @@
+import dataclasses
+import random
+import time
+import typing
+from collections import abc
+
 import pydantic_ai
+from pydantic_ai import messages as ai_messages
+from pydantic_ai import output as ai_output
+from pydantic_ai import run as ai_run
+from pydantic_ai import tools as ai_tools
 from pydantic_ai.models import openai as openai_models
 from pydantic_ai.providers import ollama as ollama_providers
+
+from soliplex import config
 
 JOKER_AGENT_PROMPT = """\
 Use the `joke_factory` to generate some jokes, then choose the best. 
@@ -50,3 +62,81 @@ def joker_agent_factory(agent_config):  # pragma NO COVER
         return r.output
 
     return joke_selection_agent
+
+
+NativeEvent = (
+    ai_messages.AgentStreamEvent | ai_run.AgentRunResultEvent[typing.Any]
+)
+MessageHistory = typing.Sequence[ai_messages.ModelMessage]
+
+
+@dataclasses.dataclass
+class FauxAgent:
+    agent_config: config.FactoryAgentConfig
+
+    output_type = None
+
+    async def run_stream_events(
+        self,
+        output_type: ai_output.OutputSpec[typing.Any] | None = None,
+        message_history: MessageHistory | None = None,
+        deferred_tool_results: pydantic_ai.DeferredToolResults | None = None,
+        deps: ai_tools.AgentDepsT = None,
+        # model=model,
+        # model_settings=model_settings,
+        # toolsets=toolsets,
+        # builtin_tools=builtin_tools,
+        # infer_name=infer_name,
+        # usage_limits=usage_limits,
+        # usage=usage,
+        **kwargs,
+    ) -> abc.AsyncIterator[NativeEvent]:
+        think_part = ai_messages.ThinkingPart("I'm thinking")
+
+        yield ai_messages.PartStartEvent(
+            index=0,
+            part=think_part,
+        )
+        last_message = message_history[-1]
+
+        time.sleep(random.uniform(0.5, 2.0))
+
+        if isinstance(last_message, ai_messages.ModelRequest):
+            ups = [
+                part
+                for part in last_message.parts
+                if isinstance(part, ai_messages.UserPromptPart)
+            ]
+            if ups:
+                up = ups[0]
+                delta = f"\n\nHmm, you asked {up.content}"
+                think_part.content += delta
+                yield ai_messages.PartDeltaEvent(
+                    index=0,
+                    delta=ai_messages.ThinkingPartDelta(
+                        content_delta=delta,
+                    ),
+                )
+
+            time.sleep(random.uniform(0.5, 2.0))
+
+        yield ai_messages.PartEndEvent(
+            index=0,
+            part=think_part,
+        )
+
+        time.sleep(random.uniform(2.5, 3.0))
+
+        text_part = ai_messages.TextPart("I don't know!")
+        yield ai_messages.PartStartEvent(
+            index=1,
+            part=text_part,
+        )
+        yield ai_messages.PartEndEvent(
+            index=1,
+            part=text_part,
+        )
+
+
+def faux_agent_factory(agent_config: config.FactoryAgentConfig):
+    return FauxAgent(agent_config)
